e7504df4f6f883615724b34e34c7b756ad51c6f2
danyflow <daniela.visa25@gmail.com>
2024-05-21 13:56:38 -0600

    dale con todo nene

---
+++ dinoplayer/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 1600
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
+		$AudioStreamPlayer2D.play()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		$AnimatedSprite2D.play("run")
+		velocity.x = direction * SPEED
+	else:
+		$AnimatedSprite2D.play("default")
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
